f6d479e577d27e1df1b658d3b861f138d285db42
SamPatt <git@sampatt.com>
2023-12-13 18:33:00 -0500

    Header and nav styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,20 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Header() {
-
-  //an example of an inline style for the nav tag for an initial layout - later additions, or 
-
-  const navStyle = {
-    display: "flex",
-    justifyContent: "space-around",
-    borderBottom: ".2rem solid black",
-    padding: ".5rem",
-    width: "90%",
-    margin: "auto",
-  };
-
   return (
     <header>
       <h1>Sam Patterson</h1>
-      <nav style={navStyle}>
-        <Link to="/">
-          <div>HOME</div>
-        </Link>
-        <Link to="/about">
+      <h4>FULLSTACK DEVELOPER</h4>
+      <nav>
+        <NavLink to="/about" activeClassName="active">
           <div>ABOUT</div>
-        </Link>
-        <Link to="/projects">
+        </NavLink>
+        <NavLink to="/projects" activeClassName="active">
           <div>PROJECTS</div>
-        </Link>
+        </NavLink>
+        <NavLink to="/contact" activeClassName="active">
+          <div>CONTACT</div>
+        </NavLink>
       </nav>
     </header>
   );
